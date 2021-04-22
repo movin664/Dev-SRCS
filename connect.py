946cf76095f8bd494fa155da8035b5624992689a
Burhanuddin M Yusufali <burhan.mufaddal786@gmail.com>
2021-04-23 02:13:02 +0530

    base64 conversations added

--- connect.py
+++ connect.py
@@ -1,6 +1,8 @@
 from flask import request, Flask
 from flask_cors import CORS
 from CoreFunction import *
+import base64
+
 app=Flask(__name__)
 CORS(app)
 @app.route("/test", methods=["POST"])
@@ -8,8 +10,10 @@ def getData():
     data=request.get_json()
     print(data)
     print(type(data))
-    
-    percentage = compare(readBlobImg(data["custId"]), data["address"])
+    image_64_decode = base64.b64decode(data["address"])
+    image_result = open('D:\\images\\image2.jpeg', 'wb') # create a writable image and write the decoding result
+    image_result.write(image_64_decode)
+    percentage = compare(readBlobImg(data["custId"]), 'D:\\images\\image2.jpeg')
     return percentage
 
 if __name__ == '__main__':
